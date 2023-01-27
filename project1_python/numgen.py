3b0b967d9b05292dc4310ffb24c9b0084964891a
Godwill Exercices Solutions <afolabigodwill24@gmail.com>
2023-01-26 23:03:46 -0600

    removing sorting code from numgen

--- project1_python/numgen.py
+++ project1_python/numgen.py
@@ -1,5 +1,5 @@
 # numgen.py
-# Module to create arrays filled with random integers and sort them
+# Module to create arrays filled with random integers
 #
 # Usage: python3 numgen.py filename quantity [rangesize]
 #
@@ -8,7 +8,6 @@
 
 import numpy
 import sys
-import quicksort
 
 
 def create_list(how_many, range_size):
@@ -70,33 +69,6 @@ def main():
     the_data = create_list(the_quantity, the_rangesize)
     write_list_to_file(the_filename, the_data)
     print(the_quantity, "values written to", the_filename)
-
-    """
-    Reading the randomly generated values from the the_filename
-    Puts them in list_of_nums list and print them out
-    """
-
-    list_of_nums = []
-    infile = open(the_filename, 'r')
-    for aline in infile:
-        anumber = int(aline)
-        list_of_nums.append(anumber)
-    print('Unordered list:')
-    print(list_of_nums)
-
-    """
-    Orders the randomly generated values in list_of_nums
-    Print the values out in the new order and stores them in a new file
-    named with the original file name '-sorted' at the end
-    """
-    size = len(list_of_nums)
-    quicksort.quickSort(list_of_nums, 0, size - 1)
-    print('Ordered list:')
-    print(list_of_nums)
-
-    the_new_filename = the_filename + '-ordered'
-    write_list_to_file(the_new_filename, list_of_nums)
-    print('Sorted values to ', the_new_filename)
 
 
 if __name__ == "__main__":
