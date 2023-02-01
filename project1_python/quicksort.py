a7e7e7769e5cf8afa7e2a6efae9343c360790f1d
Godwill Exercices Solutions <afolabigodwill24@gmail.com>
2023-01-31 21:30:22 -0600

    adding assignment and comparison count in qs

--- project1_python/quicksort.py
+++ project1_python/quicksort.py
@@ -6,8 +6,12 @@
 
 # function to find the partition position
 def partition(array, low, high):
+    comp = 0
+    assign = 0
+
     # choose the rightmost element as pivot
     pivot = array[high]
+    assign += 1
 
     # pointer for greater element
     i = low - 1
@@ -15,31 +19,44 @@ def partition(array, low, high):
     # traverse through all elements
     # compare each element with pivot
     for j in range(low, high):
+        comp += 1
         if array[j] <= pivot:
             # if element smaller than pivot is found
             # swap it with the greater element pointed by i
             i = i + 1
+            assign += 1
 
             # swapping element at i with element at j
             (array[i], array[j]) = (array[j], array[i])
+            assign += 2
 
     # swap the pivot element with the greater element specified by i
     (array[i + 1], array[high]) = (array[high], array[i + 1])
+    assign += 1
 
     # return the position from where partition is done
-    return i + 1
+    return i + 1, assign, comp
 
 
 # function to perform quicksort
 def quickSort(array, low, high):
+    assign = 0
+    comp = 0
     if low < high:
         # find pivot element such that
         # element smaller than pivot are on the left
         # element greater than pivot are on the right
-        pi = partition(array, low, high)
+        pi, ass, cc = partition(array, low, high)
+        assign += ass
+        comp += cc
 
         # recursive call on the left of pivot
-        quickSort(array, low, pi - 1)
+        ass, cc = quickSort(array, low, pi - 1)
+        assign += ass
+        comp += cc
 
         # recursive call on the right of pivot
-        quickSort(array, pi + 1, high)
+        ass, cc = quickSort(array, pi + 1, high)
+        assign += ass
+        comp += cc
+    return assign, comp
